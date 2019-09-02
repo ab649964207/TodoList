bd6a2ff9087352b7ae16c71139c9607b19100e86
cheng <649964207@qq.com>
2019-09-02 16:33:31 +0800

    添加本地到GitHub

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import Vue from 'vue';
+import App from './app.vue';
+
+
+import './assets/styles/style.styl';
+import './assets/styles/global.styl';
+
+const root = document.createElement('div')
+document.body.appendChild(root)
+new Vue({
+    render:(h) =>h(App)
+}).$mount(root)
+
+
+// new Vue({
+//     el: '#app',
+//     components: { App }
+// });
---
+++ src/todo/footer.vue
@@ -0,0 +1,28 @@
+<template>
+        <div id='footer' >
+                <span>Written by {{this.author}}</span>
+        </div>
+</template>
+
+<script>
+export default {
+   
+    data() {
+        return {
+            author:'liang'
+        }
+    }
+
+}
+    
+
+</script>
+
+
+<style lang="stylus" scoped>
+#footer
+    text-align center
+    color #fff
+    font-size 10px
+    margin-top 40px
+</style>
